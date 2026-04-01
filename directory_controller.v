3a5e2293383a53379cf0cf7fe529b93fd4c89505
Hong-Jyun Wang <hw81@pyrite.clear9.rice.edu>
2026-04-01 01:50:20 -0500

    more top level implementation

--- directory_controller.v
+++ directory_controller.v
@@ -1,13 +1,5 @@
 // Assuming serial requests and handling, no parallel execution
 
-
-// *Priority
-// TODO: Build a simple strawman L1 cache would be helpful for testing and debugging (direct mapped)
-// TODO: BUild a L2 cache as fownward main memory interface. This would be
-// more complete and full-blown compared to the strawman L1. Use set associative and bigger size
-
-
-
 module directory_controller #(
     parameter NUM_CORES = 2,
     parameter CORE_ID_BITS = 1, // number of bits representing cores (2^N cores total)
@@ -15,54 +7,74 @@ module directory_controller #(
     parameter CACHE_ENTRIES_PER_CORE = 32,
     parameter CACHE_LINE_SIZE = 64, // 64 bytes per cache line
     parameter ADDR_WIDTH = 32 // 32-bit addresses
+
+    // Address parsing
+    parameter OFFSET_BITS = $clog2(CACHE_LINE_SIZE); // Byte-offset bits in cache line
+    parameter INDEX_BITS = $clog2(CACHE_ENTRIES_PER_CORE); // Index into each directory entry
+    parameter TAG_BITS = ADDR_WIDTH - OFFSET_BITS - INDEX_BITS;
+    // Cache line entry
+    parameter LINE_WIDTH = 8 * CACHE_LINE_SIZE; // Number of data bits in a cache line
+    parameter CACHE_ENTRY_BITS = TAG_BITS + 3 + LINE_WIDTH;
 )(
     input wire clk_i,
     input wire reset_i,
-
-    // Control signals
-    input wire [CORE_ID_BITS-1:0] core_i, // The core doing the request
-    input wire [2:0] coh_req_i,
-
+    
     // input wire transaction_i,
     // input wire tx_begin_i,
     // input wire tx_end_i,
 
     // Input from L1 cache
-    input wire l1_valid_i, // L1 is presenting a real request
+    input wire l1_signal_i, // Handshake: L1 is presenting a real coherent request
+    input wire [NUM_CORES-1:0] core_i, // The core doing the request (one hot)
+    input wire [2:0] coh_req_i,
     input wire [ADDR_WIDTH-1:0] addr_i, // address from coherence request
+    output reg [(8*CACHE_LINE_SIZE)-1:0] l1_data_o, // Complete cache line being written downward (just data portion)
 
-    // Downward fetch/return interface with mem (L2)
-    input wire mem_ready_i, // Ready signal
+    // Input from L2
+    input wire l2_signal_i, // Handshake: L2 has data ready
     input wire [(8*CACHE_LINE_SIZE)-1:0] mem_rdata_i, // Complete cache line returned from lower memory
-    output reg mem_req_o, // Making downward memory request
-    output reg mem_we_o, // 0 = write req, 1 = read req
-    output reg [ADDR_WIDTH-1:0] mem_addr_o, // Data address to fetch
-    output reg [(8*CACHE_LINE_SIZE)-1:0] mem_wdata_o, // Complete cache line being written downward
 
-    // Upward fetch/return interface with L1
-    output reg l1_ready_o, // Ready signal
-    output reg [CORE_ID_BITS-1:0] l1_core_o, // Target core
+    // Output to L1 cache
+    output reg l1_signal_o, // Handshake: Ready signal for data
+    output reg [CORE_ID_BITS-1:0] l1_core_o, // Target core for data
     output reg [(8*CACHE_LINE_SIZE)-1:0] l1_data_o, // Fetched complete cache line
-    output invalidate_o,
-    output ACK_o
+    output reg l1_inv_signal_o, // Invalidate signal
+    output reg [CORE_ID_BITS-1:0] l1_inv_core_o, // Target core for invalidate signal
+    // Dont need to pass down type of downgrade bc that can be inferred from the request type
+
+    // Output to L2    
+    output reg l2_req_o, // Making downward memory request
+    output reg l2_we_o, // 1 = write req, 0 = read req
+    output reg [ADDR_WIDTH-1:0] mem_addr_o, // Data address to fetch
+    output reg [(8*CACHE_LINE_SIZE)-1:0] l2_data_o // Complete cache line being written downward (just data portion)
 );
 
 // ================ Derived Parameters ================
-localparam NUM_ENTRIES = NUM_CORES * CACHE_ENTRIES_PER_CORE; // Total number of directory entries
-localparam LINE_WIDTH  = 8 * CACHE_LINE_SIZE;
+// Address parsing
 localparam OFFSET_BITS = $clog2(CACHE_LINE_SIZE); // Byte-offset bits in cache line
-localparam INDEX_BITS  = $clog2(CACHE_ENTRIES_PER_CORE); // Index into each cache line
-localparam TAG_BITS  = ADDR_WIDTH - OFFSET_BITS - INDEX_BITS;
+localparam INDEX_BITS  = $clog2(CACHE_ENTRIES_PER_CORE); // Index into each directory entry
+localparam TAG_BITS = ADDR_WIDTH - OFFSET_BITS - INDEX_BITS;
+// Cache line entry
+localparam LINE_WIDTH = 8 * CACHE_LINE_SIZE; // Number of data bits in a cache line
+localparam CACHE_ENTRY_BITS = TAG_BITS + 3 + LINE_WIDTH;
+// Bit masks to parse input address
+localparam [ADDR_WIDTH-1:0] OFFSET_MASK = (1 << OFFSET_BITS) - 1;
+localparam [ADDR_WIDTH-1:0] INDEX_MASK = ((1 << INDEX_BITS) - 1) << OFFSET_BITS;
+localparam [ADDR_WIDTH-1:0] TAG_MASK = ((1 << TAG_BITS) - 1) << (OFFSET_BITS + INDEX_BITS);
 
 // ================ One-hot State encoding ================
 localparam [2:0] STATE_I = 3'b001; // Invalid
 localparam [2:0] STATE_S = 3'b010; // Shared
 localparam [2:0] STATE_M = 3'b100; // Modified
+localparam [2:0] STATE_MX = 3'b011; // M, fetch/invalidate in flight
+localparam [2:0] STATE_WAITING_L2 = 3'b101;
 
 // ================ Directory Entry Layout ================
 // [tag (20 bits) | state (3 bits) | Π presence vector (CORE_ID_BITS bits)]
 localparam ENTRY_WIDTH = TAG_BITS + 3 + CORE_ID_BITS;
 localparam T_IDX = NUM_CORES;
+localparam STATE_HI = CORE_ID_BITS + 2;
+localparam STATE_LO = CORE_ID_BITS;
 
 // ================ Request Types ================
 localparam REQ_LD_HIT = 3'b000;
@@ -83,35 +95,40 @@ localparam NORMAL_MODE = 1'b0;
 localparam TRANSACTION_MODE = 1'b1;
 
 
-// ================ Internal Storage ================
+
+// ================ Internal Storage and Wiring ================
 reg [ENTRY_WIDTH-1:0] directory [0:NUM_ENTRIES-1];
-reg [NUM_CORES-1:0] in_transaction;  // 1 bit per core
 reg [3:0] state;
- 
+// reg [NUM_CORES-1:0] in_transaction;  // 1 bit per core
+
 // Latched request fields — stable across all FSM states for this request
-reg [N-1:0] req_core;
-reg [1:0] req_type;
+reg [NUM_CORES-1:0] req_core;
+reg [NUM_CORES-1:0] core_state; // Core status tracking
+reg [2:0] req_type;
 reg [ADDR_WIDTH-1:0] req_addr;
- 
-// Snapshot of the directory entry taken at S_LOOKUP
-reg [ENTRY_WIDTH-1:0] cur_entry;
-// Cache line returned from memory
-reg [LINE_WIDTH-1:0] fetched_line;
+reg [ENTRY_WIDTH-1:0] cur_entry; // store Snapshot of the directory entry taken at S_LOOKUP
+reg [LINE_WIDTH-1:0] fetched_line; // store Cache line returned from memory
 
-
-// ================ Combinational helpers ================ 
 wire [2:0] cur_state = cur_entry[STATE_HI:STATE_LO]; // current directory entry's state bits
 wire [NUM_CORES-1:0] cur_pi = cur_entry[NUM_CORES-1:0]; // curren directory entry's tag bits
-
 // Directory index: bits above the cache-line offset
 wire [INDEX_BITS-1:0] dir_idx = req_addr[OFFSET_BITS + INDEX_BITS - 1 : OFFSET_BITS];
 
-// Build a one-hot presence-vector bit for a given core index
-// Converts core ID to position in core bit mask to be used when writing into directory entry
-function automatic [NUM_CORES-1:0] core_mask;
-    input [N-1:0] core;
-    core_mask = {{(NUM_CORES-1){1'b0}}, 1'b1} << core;
-endfunction
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 always @(posedge clk_i or posedge reset_i) begin
@@ -120,21 +137,80 @@ always @(posedge clk_i or posedge reset_i) begin
 
     end else begin
         // default output pulses low every cycle
-        
+
         // transaction bit-vector tracking (tx_begin_i / tx_end_i)
 
         case (state)
 
             S_IDLE: begin
-                if (l1_valid_i) begin
+                if (l1_signal_i) begin
                     // Latch the request fields stably for the rest of the FSM
                     req_core <= core_i;
-                    req_type <= normal_req_i;
+                    req_type <= coh_req_i;
                     req_addr <= addr_i;
                     cur_entry <= directory[dir_idx];
                     state <= S_PROCESS;
                 end
             end
+            S_PROCESS: begin
+                // Determine next state
+                case(req_type)
+                    REQ_LD_HIT:
+                        state <= S_LD_HIT;
+                    REQ_LD_MISS:
+                        state <= S_LD_MISS;
+                    REQ_SD_HIT:
+                        state <= S_SD_HIT;
+                    REQ_SD_MISS:
+                        state <= S_SD_MISS;
+                endcase
+            end
+
+            S_LD_HIT:  begin
+                // Should never transition into this state
+            end
+
+            S_LD_MISS: begin
+                // Check whether the requested block is in the M state in another cache (the owner)
+                if(cur_entry[CORE_ID_BITS-1:0] == 0) begin
+                    // No owner, fetch data from L2
+                    l2_req_o <= 1;
+                    l2_we_o <= 0;
+                    mem_addr_o <= req_addr;
+                    state <= STATE_WAITING_L2;
+                end else begin
+                    // NOTE CONCURRENCY ISSUE (no more?)
+                    // There is an owner, owner downgrades its L1 state to S (send out invalidate)
+                    l1_inv_core_o <= cur_entry[CORE_ID_BITS-1:0];
+                    l1_inv_signal_o <= 1;
+                    
+                    // Write the OWNER's line to L2
+                    l2_req_o <= 1;
+                    l2_we_o <= 1;
+                    l2_data_o <= l1_data_o;
+                end
+            end
+            S_SD_MISS: begin
+            
+            end
+            S_SD_HIT: begin
+            
+            end
+
+            STATE_WAITING_L2: begin
+                // Block until data from L2 is fetched
+                if(l2_signal_i) begin
+                    // Pass to L1 if received signal
+                    l1_signal_o <= 1;
+                    l1_core_o <= req_core;
+                    l1_data_o <= mem_rdata_i;
+                    state <= S_IDLE; // completed coherent request
+                end else begin
+                    state <= STATE_WAITING_L2;
+                end
+            end
+
+
 
             default: state <= S_IDLE;
 
--- l1.v
+++ l1.v
@@ -2,6 +2,9 @@
  * Each core will have its own instantiation of the l1 module
  */
 
+// TODO: Handle state downgrading based on signals l1_inv_signal_i, l1_inv_core_i
+// TODO: Actually need an ACK signal to notify the completion of a coherence request (governed by completion of some operation in L1, so will prolly be implemented here)
+
 module l1 #(
     parameter NUM_CORES = 2,
     parameter CORE_ID_BITS = 1,
@@ -14,32 +17,39 @@ module l1 #(
     input wire reset_i,
 
     // Input from the processor
-    input wire cpu_valid_i, // a real cpu request is present
+    input wire cpu_signal_i, // Handshake: a real cpu request is present
     input [ADDR_WIDTH-1:0] addr_i, // address from CPU request
     input req_i, // 0: ld, 1: sd
     input [CORE_ID_BITS-1:0] core_i, // The requesting core
 
     // Input from the directory controller
-    input l1_ready_i, // Signaled when data is ready to be sent to cpu
-    input invalidate_i, // TODO: Figure out wiring?
-    input ACK_i, // TODO: Figure out wiring?
+    input dc_signal_i, // Handshake: signaled when data is ready to be sent to cpu (ACK in paper)
+    input invalidate_i,
     input [(8*CACHE_LINE_SIZE)-1:0] l1_data_i, // Fetched complete cache line
+    input l1_inv_signal_i, // Invalidate signal
+    input [CORE_ID_BITS-1:0] l1_inv_core_i, // Target core for invalidate signal
 
     // Output to the processor
-    output [CACHE_LINE_SIZE-1:0] cpu_data_o, // data byte to output to the CPU
+    output wire cpu_ready_o, // Handshake: CPU can issue a new request when high
+    output reg cpu_signal_o, // Handshake: data is ready to be sent to CPU
+    output reg [(8*CACHE_LINE_SIZE)-1:0] cpu_data_o, // cache line output to the CPU
 
     // Output to the directory controller
-    output reg dir_valid_o // tells directory a real request is present
-    output wire [CORE_ID_BITS-1:0] core_o, // The core doing the request
+    output reg dc_signal_o, // Handshake: tells directory a real request is present
+    output wire [NUM_CORES-1:0] core_o, // The core doing the request (one hot)
     output wire [ADDR_WIDTH-1:0] addr_o, // address sent down to directory controller
     output reg [2:0] coh_req_o
+    output reg [(8*CACHE_LINE_SIZE)-1:0] l2_data_o; // data to pass to L2 for writeback
 );
 
 // ================ Derived Parameters ================
+// Address parsing
 localparam OFFSET_BITS = $clog2(CACHE_LINE_SIZE); // Byte-offset bits in cache line
-localparam INDEX_BITS = $clog2(CACHE_ENTRIES_PER_CORE); // Index into each directory entry
+localparam INDEX_BITS  = $clog2(CACHE_ENTRIES_PER_CORE); // Index into each directory entry
 localparam TAG_BITS = ADDR_WIDTH - OFFSET_BITS - INDEX_BITS;
-localparam CACHE_ENTRY_BITS = TAG_BITS + 3 + 8*CACHE_LINE_SIZE;
+// Cache line entry
+localparam LINE_WIDTH = 8 * CACHE_LINE_SIZE; // Number of data bits in a cache line
+localparam CACHE_ENTRY_BITS = TAG_BITS + 3 + LINE_WIDTH;
 // Bit masks to parse input address
 localparam [ADDR_WIDTH-1:0] OFFSET_MASK = (1 << OFFSET_BITS) - 1;
 localparam [ADDR_WIDTH-1:0] INDEX_MASK = ((1 << INDEX_BITS) - 1) << OFFSET_BITS;
@@ -48,7 +58,7 @@ localparam [ADDR_WIDTH-1:0] TAG_MASK = ((1 << TAG_BITS) - 1) << (OFFSET_BITS + I
 // ================ Request Types ================
 localparam REQ_LD_HIT = 3'b000;
 localparam REQ_LD_MISS = 3'b001;
-localparam REQ_SD_HIT  = 3'b010;
+localparam REQ_SD_HIT = 3'b010;
 localparam REQ_SD_MISS = 3'b100;
 
 // ================ Load and Store Request Types ================
@@ -60,61 +70,112 @@ localparam [2:0] STATE_I = 3'b001; // Invalid
 localparam [2:0] STATE_S = 3'b010; // Shared
 localparam [2:0] STATE_M = 3'b100; // Modified
 
-// ================ Internal Storage (Cache Line Entries) ================
-// [tag bits | state bits | transaction bit | data bits]
-reg [CACHE_ENTRY_BITS-1:0] entries [CACHE_ENTRIES_PER_CORE-1:0];
-reg hit; // hit marker
+// ================ FSM States ================
+localparam L1_IDLE = 1'b0; // Ready to accept a new CPU request
+localparam L1_WAIT = 1'b1; // Waiting on directory to service a miss
 
-// ================ Wires for Address Parsing and Argument Passing ================
+// ================ Internal Storage (Cache Line Entries) ================
+// [tag bits | state bits | data bits]
+reg [CACHE_ENTRY_BITS-1:0] entries [CACHE_ENTRIES_PER_CORE-1:0];
+
+// ================ FSM State Register ================
+// Essentially makes L1 accesses blocking for each core
+reg l1_state;
+
+// ================ Latched Miss Fields ================
+// Held stable for the entire L1_WAIT period
+reg [TAG_BITS-1:0] latched_tags;
+reg [INDEX_BITS-1:0] latched_index;
+reg latched_req;
+
+// ================ Wires for Address Parsing ================
 wire [TAG_BITS-1:0] tags;
 wire [INDEX_BITS-1:0] index;
 wire [OFFSET_BITS-1:0] offset;
-assign tag = (addr_i & TAG_MASK) >> (OFFSET_BITS + INDEX_BITS);
-assign index = (addr_i & INDEX_MASK) >> OFFSET_BITS;
+assign tags = (addr_i & TAG_MASK) >> (OFFSET_BITS + INDEX_BITS);
+assign index  = (addr_i & INDEX_MASK) >> OFFSET_BITS;
 assign offset = addr_i & OFFSET_MASK;
-assign addr_o = addr_i; // Pass down requested address and core
-assign core_o = core_i; // Pass down requesting CPU information
+// Pass through to directory
+assign addr_o = addr_i;
+assign core_o = core_i;
+// CPU can issue a new request only when IDLE
+assign cpu_ready_o = (l1_state == L1_IDLE);
 
-always@(*) begin
+// ================ Cached State Slice ================
+wire [2:0] cached_state = entries[index][CACHE_ENTRY_BITS-TAG_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS-3];
 
-    // Default to load hit
-    coh_req_o = REQ_LD_HIT;
+// ================ Hit Detection (combinational) ================
+wire hit = (entries[index][CACHE_ENTRY_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS] == tags) && (cached_state != STATE_I);
 
-    // Check if tag bits match and line is valid
-    wire [1:0] cached_state = entries[index][CACHE_ENTRY_BITS-TAG_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS-2];
-    if (entries[index][CACHE_ENTRY_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS] == tags && cached_state != STATE_I)
-        hit = 1;
-    else
-        hit = 0;
+// ================ Combinational Block ================
+// Determine request
+always @(*) begin
+    coh_req_o = REQ_LD_HIT; // default
 
-    case(req_i)
-        LOAD_REQ:
-            if(!hit)
-                coh_req_o = REQ_LD_MISS;
-        STORE_REQ:
-            coh_req_o = (hit) ? REQ_SD_HIT : REQ_SD_MISS;
-        default: 
-            coh_req_o = REQ_LD_HIT;
-    endcase
-
+    // Only evaluate requests when IDLE and a real CPU request is present
+    if (l1_state == L1_IDLE && cpu_signal_i) begin
+        case (req_i)
+            LOAD_REQ:
+                if (!hit) coh_req_o = REQ_LD_MISS;
+            STORE_REQ:
+                coh_req_o = hit ? REQ_SD_HIT : REQ_SD_MISS;
+            default:
+                coh_req_o = REQ_LD_HIT;
+        endcase
+    end
 end
 
+// ================ Sequential Block ================
 always @(posedge clk_i or posedge reset_i) begin
     if (reset_i) begin
-        dir_valid_o <= 0; // No valid request downwards
+        l1_state <= L1_IDLE;
+        dc_signal_o  <= 0;
+        cpu_signal_o <= 0;
+        cpu_data_o <= 0;
+        latched_tags <= 0;
+        latched_index <= 0;
+        latched_req <= 0;
     end else begin
-        dir_valid_o <= cpu_valid_i;
+        // Default all output pulses low each cycle
+        dc_signal_o <= 0;
+        cpu_signal_o <= 0;
 
-        // Directory has acknowledged and returned data
-        // TODO: How should we deal with this
-        if (ACK_i) begin
-            entries[index][CACHE_ENTRY_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS] <= tags;
-            entries[index][CACHE_ENTRY_BITS-TAG_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS-3] <= STATE_S;
-            entries[index][LINE_WIDTH-1:0] <= l1_data_i;
-        end
+        case (l1_state)
+            L1_IDLE: begin
+                // Respond to cpu input and send data to controller only on IDLE
+                if (cpu_signal_i) begin
+                    if (hit) begin
+                        // Hit: serve directly from cache, no need to go to directory
+                        cpu_signal_o <= 1;
+                        cpu_data_o <= entries[index][LINE_WIDTH-1:0];
+                    end else begin
+                        // Miss: latch request fields and forward to directory
+                        latched_tags <= tags;
+                        latched_index <= index;
+                        latched_req <= req_i;
+                        dc_signal_o <= 1;
+                        l1_state <= L1_WAIT;
+                        l2_data_o <= entries[index][LINE_WIDTH-1:0]; // pass data down
+                    end
+                end
+            end
+            L1_WAIT: begin
+                // Block all new CPU requests until the miss is resolved
+                // dc_signal_i should only be sent in L1_WAIT state
+                if (dc_signal_i) begin
+                    // Fill the cache entry using latched fields
+                    entries[latched_index][CACHE_ENTRY_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS] <= latched_tags;
+                    entries[latched_index][CACHE_ENTRY_BITS-TAG_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS-3] <= (latched_req == STORE_REQ) ? STATE_M : STATE_S;
+                    entries[latched_index][LINE_WIDTH-1:0] <= l1_data_i;
+                    // Return data to CPU
+                    cpu_signal_o <= 1;
+                    cpu_data_o <= l1_data_i;
+                    l1_state <= L1_IDLE;
+                end
+            end
+        endcase
 
-        // Directory is telling us to invalidate this line
-        // TODO: How should we deal with this
+        // Invalidation can arrive in either state
         if (invalidate_i) begin
             entries[index][CACHE_ENTRY_BITS-TAG_BITS-1 : CACHE_ENTRY_BITS-TAG_BITS-3] <= STATE_I;
         end
